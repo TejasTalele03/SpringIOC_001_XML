0c613cb5f76e38d8aabc53e7f875ed609012349d
TejasTalele1233 <tejastalele1233@gmail.com>
2020-07-08 17:25:13 +0530

    Changes in Spaces

--- SpringIOC_001_XML/src/main/java/com/beans/Address.java
+++ SpringIOC_001_XML/src/main/java/com/beans/Address.java
@@ -3,12 +3,12 @@ package com.beans;
 public class Address {
 
 	String landmark;
-
 	public String getLandmark() {
 		return landmark;
 	}
-
 	public void setLandmark(String landmark) {
 		this.landmark = landmark;
+
+	
 	}
 }
